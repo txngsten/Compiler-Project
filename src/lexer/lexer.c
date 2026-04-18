acf906b1c80b6e1990993a2265de873f990787b5
txngsten <oliver_wuttke@icloud.com>
2026-04-18 16:16:54 +0930

    Basic file reading

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,1 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+bool is_operator(char c) {
+    switch (c) {
+    case '+':
+        return true;
+    case '-':
+        return true;
+    case '=':
+        return true;
+    }
+
+    return false;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "No file to parse! Please ensure the relative/absolute "
+                        "file path is a CLI program argument.\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *input_file = fopen(argv[1], "r");
+
+    if (input_file == NULL) {
+        fprintf(stderr, "Error loading file, ensure file name is correct "
+                        "relative/absolute path.\n");
+        return EXIT_FAILURE;
+    }
+
+    fseek(input_file, 0, SEEK_END);
+    long file_size = ftell(input_file);
+    rewind(input_file);
+
+    char *buffer = malloc(file_size + 1);
+
+    size_t bytes_read = fread(buffer, 1, file_size, input_file);
+    buffer[bytes_read] = '\0';
+
+    fclose(input_file);
+
+    return EXIT_SUCCESS;
+}
