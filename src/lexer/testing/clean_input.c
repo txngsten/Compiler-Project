f8bac800aa5cf54ce7bbb358b5eaf62c4157c7d9
txngsten <oliver_wuttke@icloud.com>
2026-04-22 18:01:55 +0930

    Added clean_input.c test file, contains all token types

---
+++ src/lexer/testing/clean_input.c
@@ -0,0 +1,30 @@
+int x;
+char a, b;
+int arr[10];
+
+int foo(int x, char c) {
+    return x;
+}
+
+if (x) {
+    x = 0;
+} else {
+    x = 1;
+}
+
+while (x) {
+    x = x - 1;
+}
+
+for (int i = 0; i; i = i + 1) {
+    do {
+        char _underscore;
+        char _name123;
+        char camelCase;
+    } while (i);
+}
+
+int result;
+result = 42;
+result = 0;
+result = 999999;
