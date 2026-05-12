36be5194db928cecbd5a78a14af22e162f4cc821
txngsten <oliver_wuttke@icloud.com>
2026-05-12 16:20:25 +0930

    Added a bunch of test files

---
+++ src/lexer/testing/edge_cases.c
@@ -0,0 +1,18 @@
+int integer intx
+if iffy
+for formatting
+do done
+return returning
+
+a b c _ _x _1 __
+
+0 1 9 42 100 999999
+
++-=<>*/
+;,(){}[]
+
+(((((((((())))))))))
+{{{{{{{{{{}}}}}}}}}}}
+
+int x = 5;
+if(x<10){return x+1;}
---
+++ src/lexer/testing/malformed_input.c
@@ -0,0 +1,17 @@
+123abc
+456def789
+0x
+9_bad
+
+@ # $ % ^ ~ ! & ? \
+
+int @badvar = 10;
+char $name;
+return #value;
+
+123abc 456def 789ghi
+@@@ ### $$$
+
+int valid; 123bad; char also_valid;
+
+~weird~tokens~everywhere~
